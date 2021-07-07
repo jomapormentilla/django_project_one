9ff98258e93ad4d815783b9badb8da9bd2281f3b
jomapormentilla <jomapormentilla@gmail.com>
2021-07-07 10:43:36 -0400

    Create dynamic route to display visitor names

--- hello/views.py
+++ hello/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, World")
+
+def visitor(request, name):
+    return HttpResponse(f"Hello, {name}!")
