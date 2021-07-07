664a1849e5ed49a633b05da8d636bcbf7445169c
jomapormentilla <jomapormentilla@gmail.com>
2021-07-07 10:55:27 -0400

    Pass dynamic name variable to visitor route. Render as html template.

--- hello/views.py
+++ hello/views.py
@@ -6,4 +6,6 @@ def index(request):
     return render(request, "hello/index.html")
 
 def visitor(request, name):
-    return HttpResponse(f"Hello, {name.capitalize()}!")
+    return render(request, 'hello/visitor.html', {
+        "name": name.capitalize()
+    })
