80be8e150d5bb51c84e8fbf454399d7b7ef1ab6d
jomapormentilla <jomapormentilla@gmail.com>
2021-07-07 14:30:53 -0400

    Migrate default django tables

--- todolist/views.py
+++ todolist/views.py
@@ -1,19 +1,34 @@
 from django import forms
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
-tasks = ["test"]
+
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
-    priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
 
 # Create your views here.
 def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
+
     return render(request, "todolist/index.html", {
-        "tasks": tasks
+        "tasks": request.session["tasks"]
     })
 
 def add(request):
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+            return HttpResponseRedirect(reverse("todolist:index"))
+        else:
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
+
     return render(request, "todolist/add.html", {
         "form": NewTaskForm()
     })
